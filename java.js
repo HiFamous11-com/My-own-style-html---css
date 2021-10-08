e448be022ec312b9b1a069a478ce50b8ad19bec1
HiFamous11-com <80319540+HiFamous11-com@users.noreply.github.com>
2021-10-08 18:19:25 +0530

    l

---
+++ java.js
@@ -0,0 +1,91 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://use.fontawesome.com/9da9b46707.js"></script>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="comments-area">
+      <h4>03 Comments</h4>
+      <div class="comment-list">
+        <div class="single-comment justify-content-between d-flex">
+          <div class="user justify-content-between d-flex">
+            <div class="thumb">
+              <i class="fa fa-user" style="font-size:30px; width:30px;"></i>
+            </div>
+            <div class="desc">
+              <h5>Emilly Blunt</h5>
+              <p class="date">December 4, 2017 at 3:12 pm </p>
+              <p class="comment">
+                Never say goodbye till the end comes!
+              </p>
+            </div>
+          </div>
+          <div class="reply-btn">
+            <a href="" class="btn-reply text-uppercase">reply</a>
+          </div>
+        </div>
+      </div>
+      <div class="comment-list left-padding">
+        <div class="single-comment justify-content-between d-flex">
+          <div class="user justify-content-between d-flex">
+            <div class="thumb">
+              <i class="fa fa-user" style="font-size:30px; width:30px;"></i>
+            </div>
+            <div class="desc">
+              <h5>Elsie Cunningham</h5>
+              <p class="date">December 4, 2017 at 3:12 pm </p>
+              <p class="comment">
+                I am Reply of above comment.
+              </p>
+            </div>
+          </div>
+          <div class="reply-btn">
+            <a href="" class="btn-reply text-uppercase">reply</a>
+          </div>
+        </div>
+      </div>
+      <div id="divider-transparent"></div>
+      <div class="comment-list">
+        <div class="single-comment justify-content-between d-flex">
+          <div class="user justify-content-between d-flex">
+            <div class="thumb">
+              <i class="fa fa-user" style="font-size:30px; width:30px;"></i>
+            </div>
+            <div class="desc">
+              <h5>Ina Hayes</h5>
+              <p class="date">December 4, 2017 at 3:12 pm </p>
+              <p class="comment">
+                Never say goodbye till the end comes!
+              </p>
+            </div>
+          </div>
+          <div class="reply-btn">
+            <a href="" class="btn-reply text-uppercase">reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="comment-form">
+      <h4>Leave a Comment/Reply</h4>
+      <form action="#">
+        <div class="row">
+          <div class="offset-lg-2 col-lg-4 col-md-4 col-sm-5">
+            <input type="text" class="form-control" placeholder="Full Name *" />
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-5">
+            <input type="text" class="form-control" placeholder="Email *" />
+          </div>
+        </div><br />
+        <div class="row">
+          <div class="offset-lg-2 col-lg-8 col-md-8 col-sm-10">
+            <textarea rows="5" class="form-control" placeholder="Comment"></textarea>
+          </div>
+        </div><br />
+        <div class="form-group" style="text-align: center;">
+          <input type="submit" value="Submit" class="btn btn-primary py-3 px-5 submit-comment">
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
